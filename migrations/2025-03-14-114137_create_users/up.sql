762e337366c39664546f80c956883ff6aae1d660
XAVIER Clément Antoine <clement-antoine.xavier@epitech.eu>
2025-03-14 17:24:12 +0400

    Fix: Update email and password lenght in SQL schema

--- migrations/2025-03-14-114137_create_users/up.sql
+++ migrations/2025-03-14-114137_create_users/up.sql
@@ -3,7 +3,7 @@ CREATE TABLE
     users (
         id SERIAL PRIMARY KEY,
         email VARCHAR(320) UNIQUE NOT NULL,
-        password_hash TEXT NOT NULL,
+        password_hash VARCHAR(72) NOT NULL,
         created_at TIMESTAMP NOT NULL DEFAULT NOW(),
         updated_at TIMESTAMP NOT NULL DEFAULT NOW()
     );
