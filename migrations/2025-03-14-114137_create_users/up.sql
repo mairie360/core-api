5664735ad71e465ee0c011c4f130c2df0115c39e
XAVIER Clément Antoine <clement-antoine.xavier@epitech.eu>
2025-03-15 14:35:05 +0400

    Fix: Remove users SQL schema

--- migrations/2025-03-14-114137_create_users/up.sql
+++
@@ -1,9 +0,0 @@
--- Your SQL goes here
-CREATE TABLE
-    users (
-        id SERIAL PRIMARY KEY,
-        email VARCHAR(320) UNIQUE NOT NULL,
-        password_hash VARCHAR(72) NOT NULL,
-        created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-        updated_at TIMESTAMP NOT NULL DEFAULT NOW()
-    );
